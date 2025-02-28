484306089cc9131e46ca359c4e208ef8000e6ed0
Alfredoss Naguaty <alfredo.naguatt@gmail.com>
2025-02-28 15:13:03 -0500

    cambios a nuevo server

--- src/js/http.js
+++ src/js/http.js
@@ -2,7 +2,7 @@ import storage from './storage';
 import utils from './utils';
 
 const getServer = (url) => {
-  const api = 'http://35.92.50.147:3000';
+  const api = 'http://bootcamp.salesup.com:4432';
   if (url.includes('http')) { return url; }
 
   return api + url;
